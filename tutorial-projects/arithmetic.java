42f4988a98a026267d76637079126e57087c3bea
Agastya Naik <agastya27naik@gmail.com>
2025-07-12 11:15:12 +0000

    commit_11

--- tutorial-projects/arithmetic.java
+++ tutorial-projects/arithmetic.java
@@ -1,7 +1,7 @@
 // Arithmetic Operations in Java:
 // Java supports basic arithmetic operations: addition, subtraction, multiplication, division, and modulus.
 
-public class ArithmeticExample 
+public class ArithmeticExample {
     public static void main(String[] args) {
         int a = 15;
         int b = 4;
@@ -11,3 +11,9 @@ public class ArithmeticExample
         System.out.println("Multiplication: " + (a * b));   // 60
         System.out.println("Division: " + (a / b));         // 3
         System.out.println("Modulus: " + (a % b));          // 3
+    }
+}
+// This code demonstrates how to perform basic arithmetic operations in Java.
+// The output will show the results of each operation performed on the integers a and b.
+// You can run this code in any Java environment to see the results.
+// Make sure to compile the code using `javac ArithmeticExample.java` and run it with
